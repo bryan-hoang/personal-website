90b645a954b27bb39c238b335f0073b2d7ccbfa9
Bryan Hoang <bryan.c.hoang@gmail.com>
2020-12-24 17:34:29 -0500

    feat: change footer image
    Also update testing snapshot

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
 import { Flex, FlexProps } from '@chakra-ui/react';
 import { FunctionComponent } from 'react';
 
-export const Footer: FunctionComponent<FlexProps> = (props) => (
+const Footer: FunctionComponent<FlexProps> = (props) => (
   <Flex as="footer" py="8rem" {...props} />
 );
+
+export default Footer;
