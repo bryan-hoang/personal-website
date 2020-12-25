a7d18a66f156cd264ba3df6c863697ff69d0e5ac
Bryan Hoang <bryan.c.hoang@gmail.com>
2020-12-25 13:16:24 -0500

    chore: improve npm scripts and githooks

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![CI](https://github.com/bryan-hoang/personal-website/workflows/CI/badge.svg?branch=main)
 
-My personal website hosted at https://bryanhoang.dev/. It's built with:
+My personal website hosted at <https://bryanhoang.dev/>. It's built with:
 
 - Next.js
 - Chakra UI
--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,16 @@
+const typeCheckCommand = () => 'npm run type-check';
+const eslintCommand = 'eslint --cache --ext ts,tsx,js';
+const prettierCommand = 'prettier -c';
+const markdownlintCommand = 'markdownlint **/*.md --ignore node_modules';
+
+const typescriptFiles = '*.@(ts|tsx)';
+const eslintableFiles = '*.@(ts|tsx|js)';
+const markdownFiles = '*.@(md)';
+const prettierableFiles = '*.@(md|json|svg|yaml)';
+
 module.exports = {
-  '*.@(ts|tsx)': [() => 'npm run type-check', 'npm run lint'],
-  '*.@(json|md|svg)': ['npm run format'],
+  [typescriptFiles]: [typeCheckCommand],
+  [eslintableFiles]: [eslintCommand],
+  [markdownFiles]: [markdownlintCommand],
+  [prettierableFiles]: [prettierCommand],
 };
